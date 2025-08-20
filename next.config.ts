66d5df3674a201c7d46140865d2cbea258c6d641
Rudzani Mudau <ndaedzomu101@gmail.com>
2025-08-20 18:54:27 +0200

    y

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Enables static HTML export
-  output: "export",
-
-  // Trailing slash needed for static export URLs
-  trailingSlash: true,
-
-  // Optional: for images to work when exporting static HTML
-  images: {
-    unoptimized: true,
-  },
+ //
 };
 
 export default nextConfig;
